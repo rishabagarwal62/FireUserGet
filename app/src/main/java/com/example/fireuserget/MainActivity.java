cbe22e012feb1d88040260051bb46ed27e56090b
rishabagarwal62 <rishabagarwal62@gmail.com>
2020-07-03 20:40:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/fireuserget/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.fireuserget;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    String name;
+
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference reference= database.getReference();
+    DatabaseReference userRef= reference.child("Users");
+
+    ListView listView;
+    ArrayList<String> usernames = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView= findViewById(R.id.listView);
+
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, usernames);
+        listView.setAdapter(arrayAdapter);
+
+        userRef.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                String value = snapshot.getValue().toString();
+
+                try {
+                    JSONObject object= new JSONObject(value);
+                    name = object.getString("Email");
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                usernames.add(name);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+    }
+}
